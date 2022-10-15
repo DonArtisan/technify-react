c17d1a35ce7969790243d8636028568bf31e9a1c
Maria Gomez <16159494+mariabug@users.noreply.github.com>
2022-10-15 17:56:39 -0600

    feat: added authcontext

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,75 @@
+// export default function AuthContext() {
+
+import { createContext, useContext, useReducer } from 'react';
+import { AUTH_TOKEN } from '../constants';
+
+const InitialState = {
+  user: null,
+};
+
+const AuthContext = createContext({
+  user: null,
+  login: (userData) => {},
+  logout: () => {},
+});
+
+function authReducer(state, action) {
+  switch (action.type) {
+    case 'LOGIN':
+      return {
+        ...state,
+        user: action.payload,
+      };
+    case 'LOGOUT':
+      return {
+        ...state,
+        user: null,
+      };
+    case 'SIGNIN':
+      return {
+        ...state,
+        user: null,
+      };
+    default:
+      return state;
+  }
+}
+
+function AuthProvider(props) {
+  const [state, dispatch] = useReducer(authReducer, InitialState);
+
+  const login = (userData) => {
+    localStorage.setItem(AUTH_TOKEN, userData.userToken);
+    dispatch({
+      type: 'LOGIN',
+      payload: userData,
+    });
+  };
+
+  const signin = (userData) => {
+    localStorage.setItem(AUTH_TOKEN, userData.userToken);
+    console.log(userData);
+    dispatch({
+      type: 'SIGNIN',
+      payload: userData,
+    });
+  };
+
+  function logout() {
+    localStorage.removeItem(AUTH_TOKEN);
+    dispatch({ type: 'LOGOUT' });
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{ user: state.user, login, logout, signin }}
+      {...props}
+    />
+  );
+}
+
+function useAuth() {
+  return useContext(AuthContext);
+}
+
+export { useAuth, AuthProvider };
