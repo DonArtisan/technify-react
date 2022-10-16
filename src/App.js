dd0fa831cbcc2b0d9a0a43350155e23911a45469
Maria Gomez <16159494+mariabug@users.noreply.github.com>
2022-10-15 19:32:15 -0600

    feat: added login route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/home/Home';
+import { Login } from './pages/login';
 import { Register } from './pages/register';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      {/* <Route element={<Login />} path="login" /> */}
+      <Route element={<Login />} path="login" />
       <Route element={<Register />} path="register" />
       {/* <Route path="expenses" element={<Expenses />} /> */}
       {/* <Route path="invoices" element={<Invoices />} /> */}
