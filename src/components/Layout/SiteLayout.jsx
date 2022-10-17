fdd1edd505dca70ce13062c938366f4cb946e45e
Maria Gomez <16159494+mariabug@users.noreply.github.com>
2022-10-16 19:37:06 -0600

    added padding to the sitelayout

--- src/components/Layout/SiteLayout.jsx
+++ src/components/Layout/SiteLayout.jsx
@@ -1,11 +1,14 @@
-import { Box } from '@chakra-ui/react';
+import { Flex } from '@chakra-ui/react';
 import Footer from '../Footer/Footer';
 import NavigationBar from '../NavigationBar/NavigationBar';
 export default function SiteLayout({ children }) {
   return (
     <>
       <NavigationBar />
-      {children}
+
+      <Flex direction="column" paddingX={60}>
+        {children}
+      </Flex>
       <Footer />
     </>
   );
