58f3c2211731f8b55baeb4ee0916ac44eebd951c
Maria Gomez <16159494+mariabug@users.noreply.github.com>
2022-10-17 09:07:29 -0600

    updated components

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,23 @@
-import {Box, Divider, Flex, Heading, Link} from '@chakra-ui/react'
+import {
+  Box,
+  Divider,
+  Flex,
+  Heading,
+  HStack,
+  Icon,
+  Link,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from '@chakra-ui/react'
+import {FiHeadphones} from 'react-icons/fi'
+import {IoIosPricetag, IoMdContact} from 'react-icons/io'
 import {Link as ReactRouterLink} from 'react-router-dom'
 import ProductCard from '../../components/ProductCard/ProductCard'
+import CategoriesList from './components/CategoriesList/CategoriesList'
 
 export default function Home() {
   return (
@@ -35,7 +52,126 @@ export default function Home() {
             <ProductCard key={index} />
           ))}
         </Flex>
-        <Divider />
+        <Divider marginBlock="20px" />
+        <Flex marginBlockEnd="38px">
+          <CategoriesList title="Custom Builds" />
+        </Flex>
+        <Box>
+          <Tabs>
+            <TabList>
+              <Tab>MSI GS Series</Tab>
+              <Tab>MSI GT Series</Tab>
+              <Tab>MSI GL Series</Tab>
+            </TabList>
+            <TabPanels>
+              <TabPanel paddingY="20px" paddingX="0">
+                <CategoriesList title="MSI Laptops" />
+              </TabPanel>
+              <TabPanel paddingY="20px" paddingX="0">
+                <CategoriesList title="MSI Laptops" />
+              </TabPanel>
+              <TabPanel paddingY="20px" paddingX="0">
+                <CategoriesList title="MSI Laptops" />
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </Box>
+        <Box marginBlockEnd="30px">
+          <Tabs>
+            <TabList>
+              <Tab>MSI Infinute Series</Tab>
+              <Tab>MSI Triden</Tab>
+              <Tab>MSI GL Series</Tab>
+            </TabList>
+            <TabPanels>
+              <TabPanel paddingBlock="20px 0" paddingX="0">
+                <CategoriesList title="Desktops" />
+              </TabPanel>
+              <TabPanel paddingBlock="20px 0" paddingX="0">
+                <CategoriesList title="Desktops" />
+              </TabPanel>
+              <TabPanel paddingBlock="20px 0" paddingX="0">
+                <CategoriesList title="Desktops" />
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </Box>
+        <Flex>
+          <CategoriesList title="Gaming Monitors" />
+        </Flex>
+        <Divider marginBlockStart="30px" />
+        <HStack spacing="100px" justify="center" marginBlock="40px">
+          <Flex
+            width="265px"
+            direction="column"
+            justify="center"
+            align="center"
+          >
+            <Flex
+              height="65px"
+              width="65px"
+              bgColor="#0156FF"
+              borderRadius="50%"
+              justify="center"
+              align="center"
+            >
+              <Icon as={FiHeadphones} color="white" w={8} h={8} />
+            </Flex>
+            <Heading as="h4" fontSize="20px" marginBlock="24px 13px">
+              Product Support
+            </Heading>
+            <Text textAlign="center" color="gray.600">
+              Up to 3 years on-site warranty available for your peace of mind.
+            </Text>
+          </Flex>
+          <Flex
+            width="265px"
+            direction="column"
+            justify="center"
+            align="center"
+          >
+            <Flex
+              height="65px"
+              width="65px"
+              bgColor="#0156FF"
+              borderRadius="50%"
+              justify="center"
+              align="center"
+            >
+              <Icon as={IoMdContact} color="white" w={8} h={8} />
+            </Flex>
+            <Heading as="h4" fontSize="20px" marginBlock="24px 13px">
+              Personal Account
+            </Heading>
+            <Text textAlign="center" color="gray.600">
+              With big discounts, free delivery and a dedicated support
+              specialist.
+            </Text>
+          </Flex>
+          <Flex
+            width="265px"
+            direction="column"
+            justify="center"
+            align="center"
+          >
+            <Flex
+              height="65px"
+              width="65px"
+              bgColor="#0156FF"
+              borderRadius="50%"
+              justify="center"
+              align="center"
+            >
+              <Icon as={IoIosPricetag} color="white" w={8} h={8} />
+            </Flex>
+            <Heading as="h4" fontSize="20px" marginBlock="24px 13px">
+              Amazing Savings
+            </Heading>
+            <Text textAlign="center" color="gray.600">
+              Up to 70% off new Products, you can be sure of the best price.
+            </Text>
+          </Flex>
+        </HStack>
       </Flex>
     </>
   )
--- src/pages/home/components/CategoriesList/CategoriesList.jsx
+++ src/pages/home/components/CategoriesList/CategoriesList.jsx
@@ -1,11 +1,31 @@
-import {Flex} from '@chakra-ui/react'
-import {Box} from '@chakra-ui/react'
+import {Flex, Heading, Link} from '@chakra-ui/react'
+import {Link as ReactRouterLink} from 'react-router-dom'
 import ProductCard from '../../../../components/ProductCard/ProductCard'
 
-export default function CategoriesList(params) {
+export default function CategoriesList({title}) {
   return (
     <Flex>
-      <Box width="233px" height="350px" backgroundColor="gray.200" />
+      <Flex
+        direction="column"
+        align="center"
+        width="233px"
+        height="350px"
+        backgroundColor="gray.200"
+        textAlign="center"
+      >
+        <Heading marginBlock="150px 100px" as="h4" fontSize="22px">
+          {title}
+        </Heading>
+        <Link
+          textDecor="underline"
+          fontWeight="semibold"
+          as={ReactRouterLink}
+          to="/"
+        >
+          See all Products
+        </Link>
+      </Flex>
+
       {[...new Array(5)].map((product, index) => (
         <ProductCard key={index} />
       ))}
