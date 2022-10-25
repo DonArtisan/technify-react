4d9a74bf9464f39b925043866f848d20bb559ae5
Maria Gomez <16159494+mariabug@users.noreply.github.com>
2022-10-24 18:36:23 -0600

    fixed missing file

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,3 @@
-import { layout } from '@chakra-ui/react';
-import SiteLayout from '../../components/Layout/SiteLayout';
-
 export default function Home() {
-  return (
-    <SiteLayout>
-      <div>Hola</div>
-    </SiteLayout>
-  );
+  return <div>Hola</div>;
 }
