9e5cab034850f2176ba79223758afd68ab30ca52
Carlos-vargs <cvargaslopez769@gmail.com>
2022-11-08 16:27:11 -0600

    ci: add wrap suspense

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
+import {Suspense} from 'react'
 import {Route, Routes} from 'react-router-dom'
 import './App.css'
 import Home from './pages/home'
 import {Login} from './pages/login'
+import Products from './pages/products'
 import {Register} from './pages/register'
-import ShoppingCart from './pages/shopping-cart';
+import ShoppingCart from './pages/shopping-cart'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route
+        path="/"
+        element={
+          <Suspense fallback={'loading...'}>
+            <Home />
+          </Suspense>
+        }
+      />
       <Route element={<Login />} path="login" />
       <Route element={<Register />} path="register" />
       <Route element={<ShoppingCart />} path="shopping-cart" />
+      <Route
+        element={
+          <Suspense fallback={'loading...'}>
+            <Products />
+          </Suspense>
+        }
+        path="products"
+      />
       {/* <Route path="expenses" element={<Expenses />} /> */}
       {/* <Route path="invoices" element={<Invoices />} /> */}
     </Routes>
