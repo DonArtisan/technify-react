8d2b0f141a18a5152c07a85768a478c8de179df1
mariabug <16159494+mariabug@users.noreply.github.com>
2022-11-07 20:30:19 -0600

    chore: updated graphql files

---
+++ src/components/__generated__/CategoriesListQuery.graphql.js
@@ -0,0 +1,140 @@
+/**
+ * @generated SignedSource<<65befc672c6262af8e07a2a374894224>>
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* eslint-disable */
+
+'use strict';
+
+var node = (function(){
+var v0 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "id",
+  "storageKey": null
+},
+v1 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "name",
+  "storageKey": null
+},
+v2 = [
+  {
+    "alias": null,
+    "args": [
+      {
+        "kind": "Literal",
+        "name": "first",
+        "value": 4
+      }
+    ],
+    "concreteType": "CategoryConnection",
+    "kind": "LinkedField",
+    "name": "categories",
+    "plural": false,
+    "selections": [
+      {
+        "alias": null,
+        "args": null,
+        "concreteType": "CategoryEdge",
+        "kind": "LinkedField",
+        "name": "edges",
+        "plural": true,
+        "selections": [
+          {
+            "alias": null,
+            "args": null,
+            "concreteType": "Category",
+            "kind": "LinkedField",
+            "name": "node",
+            "plural": false,
+            "selections": [
+              (v0/*: any*/),
+              (v1/*: any*/),
+              {
+                "alias": null,
+                "args": [
+                  {
+                    "kind": "Literal",
+                    "name": "first",
+                    "value": 6
+                  }
+                ],
+                "concreteType": "ProductConnection",
+                "kind": "LinkedField",
+                "name": "products",
+                "plural": false,
+                "selections": [
+                  {
+                    "alias": null,
+                    "args": null,
+                    "concreteType": "ProductEdge",
+                    "kind": "LinkedField",
+                    "name": "edges",
+                    "plural": true,
+                    "selections": [
+                      {
+                        "alias": null,
+                        "args": null,
+                        "concreteType": "Product",
+                        "kind": "LinkedField",
+                        "name": "node",
+                        "plural": false,
+                        "selections": [
+                          (v0/*: any*/),
+                          (v1/*: any*/)
+                        ],
+                        "storageKey": null
+                      }
+                    ],
+                    "storageKey": null
+                  }
+                ],
+                "storageKey": "products(first:6)"
+              }
+            ],
+            "storageKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ],
+    "storageKey": "categories(first:4)"
+  }
+];
+return {
+  "fragment": {
+    "argumentDefinitions": [],
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "CategoriesListQuery",
+    "selections": (v2/*: any*/),
+    "type": "Query",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": [],
+    "kind": "Operation",
+    "name": "CategoriesListQuery",
+    "selections": (v2/*: any*/)
+  },
+  "params": {
+    "cacheID": "87e309254cafd82473f4d1561659fa46",
+    "id": null,
+    "metadata": {},
+    "name": "CategoriesListQuery",
+    "operationKind": "query",
+    "text": "query CategoriesListQuery {\n  categories(first: 4) {\n    edges {\n      node {\n        id\n        name\n        products(first: 6) {\n          edges {\n            node {\n              id\n              name\n            }\n          }\n        }\n      }\n    }\n  }\n}\n"
+  }
+};
+})();
+
+node.hash = "73d1539cfa6ddeb444cefa29b9950636";
+
+module.exports = node;
