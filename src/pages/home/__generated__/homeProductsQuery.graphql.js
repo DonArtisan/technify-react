53538af98ab0766fc75c30f898eddbd76a75b462
mariabug <16159494+mariabug@users.noreply.github.com>
2022-11-07 19:02:01 -0600

    updated graphql generated schemas

---
+++ src/pages/home/__generated__/homeProductsQuery.graphql.js
@@ -0,0 +1,142 @@
+/**
+ * @generated SignedSource<<b2c50fb950352ad4ab9331fa8280317b>>
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* eslint-disable */
+
+'use strict';
+
+var node = (function(){
+var v0 = [
+  {
+    "kind": "Literal",
+    "name": "first",
+    "value": 5
+  }
+],
+v1 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "name",
+  "storageKey": null
+},
+v2 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "description",
+  "storageKey": null
+};
+return {
+  "fragment": {
+    "argumentDefinitions": [],
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "homeProductsQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": (v0/*: any*/),
+        "concreteType": "ProductConnection",
+        "kind": "LinkedField",
+        "name": "products",
+        "plural": false,
+        "selections": [
+          {
+            "alias": null,
+            "args": null,
+            "concreteType": "ProductEdge",
+            "kind": "LinkedField",
+            "name": "edges",
+            "plural": true,
+            "selections": [
+              {
+                "alias": null,
+                "args": null,
+                "concreteType": "Product",
+                "kind": "LinkedField",
+                "name": "node",
+                "plural": false,
+                "selections": [
+                  (v1/*: any*/),
+                  (v2/*: any*/)
+                ],
+                "storageKey": null
+              }
+            ],
+            "storageKey": null
+          }
+        ],
+        "storageKey": "products(first:5)"
+      }
+    ],
+    "type": "Query",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": [],
+    "kind": "Operation",
+    "name": "homeProductsQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": (v0/*: any*/),
+        "concreteType": "ProductConnection",
+        "kind": "LinkedField",
+        "name": "products",
+        "plural": false,
+        "selections": [
+          {
+            "alias": null,
+            "args": null,
+            "concreteType": "ProductEdge",
+            "kind": "LinkedField",
+            "name": "edges",
+            "plural": true,
+            "selections": [
+              {
+                "alias": null,
+                "args": null,
+                "concreteType": "Product",
+                "kind": "LinkedField",
+                "name": "node",
+                "plural": false,
+                "selections": [
+                  (v1/*: any*/),
+                  (v2/*: any*/),
+                  {
+                    "alias": null,
+                    "args": null,
+                    "kind": "ScalarField",
+                    "name": "id",
+                    "storageKey": null
+                  }
+                ],
+                "storageKey": null
+              }
+            ],
+            "storageKey": null
+          }
+        ],
+        "storageKey": "products(first:5)"
+      }
+    ]
+  },
+  "params": {
+    "cacheID": "00ebef940d5da6b9219099eb428e155d",
+    "id": null,
+    "metadata": {},
+    "name": "homeProductsQuery",
+    "operationKind": "query",
+    "text": "query homeProductsQuery {\n  products(first: 5) {\n    edges {\n      node {\n        name\n        description\n        id\n      }\n    }\n  }\n}\n"
+  }
+};
+})();
+
+node.hash = "4b15f52e32c69a730e3c7a17ed84a396";
+
+module.exports = node;
