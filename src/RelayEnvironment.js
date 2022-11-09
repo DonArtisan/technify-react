fec2c7c10d3b17606e51f1bc26053f1ccc050db1
mariabug <16159494+mariabug@users.noreply.github.com>
2022-11-09 08:43:58 -0600

    updated relay environment

--- src/RelayEnvironment.js
+++ src/RelayEnvironment.js
@@ -1,21 +1,24 @@
-import { Environment, Network, RecordSource, Store } from 'relay-runtime';
-import { AUTH_TOKEN } from './constants';
+import {Environment, Network, RecordSource, Store} from 'relay-runtime'
+import {AUTH_TOKEN} from './constants'
 async function fetchGraphQL(params, variables) {
-  console.log(variables);
+  const accessToken = localStorage.getItem(AUTH_TOKEN)
+  const headers = {'Content-Type': 'application/json'}
+
+  if (accessToken) {
+    headers.Authorization = `Bearer ${accessToken}`
+  }
+
   const response = await fetch('http://technify-api.test/graphql', {
     body: JSON.stringify({
       query: params.text,
       variables,
     }),
-    headers: {
-      Authorization: `bearer ${AUTH_TOKEN}`,
-      'Content-Type': 'application/json',
-    },
+    headers,
     method: 'POST',
-  });
-  return response.json();
+  })
+  return response.json()
 }
 export default new Environment({
   network: Network.create(fetchGraphQL),
   store: new Store(new RecordSource()),
-});
+})
