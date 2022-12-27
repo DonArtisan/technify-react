2a41624d02cf57599c13a1dae879409131b6cb8c
mariabug <16159494+mariabug@users.noreply.github.com>
2022-12-27 14:04:00 -0600

    changed relay configuration and started used graphql from relay react instead of babel

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -13,10 +13,9 @@ import {
   Input,
   Link as LinkC,
 } from '@chakra-ui/react'
-import {graphql} from 'babel-plugin-relay/macro'
 import {Field, Form, Formik} from 'formik'
 import {useEffect} from 'react'
-import {useMutation} from 'react-relay'
+import {graphql, useMutation} from 'react-relay'
 import {Link} from 'react-router-dom'
 import {useNavigate} from 'react-router-dom'
 import * as Yup from 'yup'
